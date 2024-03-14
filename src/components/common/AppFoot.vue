fd3d97826f0b4c13d2ee2f43cdc7443d447e1851
youngbull <1211794295@qq.com>
2024-03-15 00:52:33 +0800

    first commit

---
+++ src/components/common/AppFoot.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="foot-container">
+<!--        <div class="author">二手物品交易平台</div>-->
+        <p align="center"><a href="https://github.com/youngbull0616" target="_blank"><font color="blue">design by youngbull0616</font></a></p>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        name: "Foot"
+    }
+</script>
+
+<style scoped>
+    .foot-container {
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        height: 40px;
+        padding-top: 10px;
+    }
+
+    .author {
+        color: #999999;
+        font-size: 14px;
+    }
+</style>
